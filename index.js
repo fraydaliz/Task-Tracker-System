f9137bdea19716af3ef723aadda5e8d344d3d32a
Fraydaliz Carpio <carpiofraydaliz19@gmail.com>
2025-07-21 18:03:30 -0400

    cors enable

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { employee } from "./routes/employeeRoutes.js";
 import { task } from "./routes/tasksRoutes";
+import cors from 'cors'
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+app.use(express.json())
+app.use(cors())
 
 app.use("/api", employee);
 
