76eb73b4e551ec0028e071813ac289d36e3fe694
Fraydaliz Carpio <carpiofraydaliz19@gmail.com>
2025-07-31 09:59:04 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 import express from "express";
-import { employee } from "./routes/employeeRoutes.js";
-import { task } from "./routes/tasksRoutes";
-import cors from 'cors'
-
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import { employee } from "./routes/employeeRoutes.js";
+import task from "./routes/tasksRoutes.js";
+import cors from "cors";
+import fs from "fs";
+import https from "https";
+
+const enviroment = process.env.NODE_ENVIROMENT;
 const port = 3000;
 
-app.use(express.json())
-app.use(cors())
+if (enviroment === "production") {
+  port = 443;
+}
 
+app.use(express.json());
+app.use(cors());
 app.use("/api", employee);
-
 app.use("/api", task);
 
-app.listen(port, () => {
-  console.log(`listening in port ${port}`);
-});
+if (enviroment === "production") {
+  const options = {
+    key: fs.readFileSync(
+      "/etc/letsencrypt/live/ssl-fray-backend.codex-p4-2025.click/privkey.pem"
+    ),
+    cert: fs.readFileSync(
+      "/etc/letsencrypt/live/ssl-fray-backend.codex-p4-2025.click/fullchain.pem"
+    ),
+  };
+  https.createServer(options, app).listen(port, () => {
+    console.log("HTTPS server is running on port 443");
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`listening in port ${port}`);
+  });
+}
